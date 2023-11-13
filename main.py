58d9eff7991856bf82d92be94c92f5af1c05b27b
Padmaj Nikam <112931046+Padmaj-Nikam@users.noreply.github.com>
2023-11-13 17:39:47 +0530

    Making Classes and Objects

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+#Class: A class is a blueprint for creating objects. It defines a set of attributes (data members) and methods (functions) that the objects of the class will have.
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
 
+#Object: An object is an instance of a class. It is a concrete entity created based on the class, and it can have its own unique attributes and behaviors.
+car1 = Car("Tesla", "Model-S")
+car2 = Car("BMW", "Series-7")
+
+#Printing a particular attribute
+print(car1.model, car2.model)
