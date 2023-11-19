e633478a0b8a0b5408e71a077bfd7645f086f280
Padmaj Nikam <112931046+Padmaj-Nikam@users.noreply.github.com>
2023-11-19 13:56:11 +0530

    Added Spherical operations.

--- radial-operations.py
+++ radial-operations.py
@@ -11,15 +11,26 @@ class Circle:
         return 2 * 3.14 * self.radius
 
     def area(self):
-        return pi * self.radius * self.radius
+        return pi * self.radius**2
 
 
 class Sphere:
     def __init__(self, radius):
         self.radius = radius
 
+    def surface_area(self):
+        return 4 * pi * self.radius**2
+
+    def volume(self):
+        return 4 / 3 * pi * self.radius**3
+
 
 radi1 = Circle(float(input("Enter Radius here: ")))
 
 print(radi1.perimeter())
 print(radi1.area())
+
+sphere1 = Sphere(float(input("Enter radius here: ")))
+
+print(sphere1.surface_area())
+print(sphere1.volume())
